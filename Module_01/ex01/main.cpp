99f1844bd664fa81e5d4f235c15e966ac8c38e6a
Hamza Fl <Dridex@Hamza-Fl.localdomain>
2025-09-27 14:22:23 +0100

    add ex00,ex01,ex02

---
+++ Module_01/ex01/main.cpp
@@ -0,0 +1,14 @@
+#include "Zombie.hpp"
+
+int main() {
+
+    int N = 5;
+    Zombie* horde = zombieHorde(N, "HordeZombie");
+    if (!horde)
+        return 1;
+    for (int i = 0; i < N; ++i)
+        horde[i].announce();
+
+    delete [] horde;
+    return 0;
+}
